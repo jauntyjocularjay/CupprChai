ef607bbc1b486647ab64a586e6c761aaf6eb0010
GeorgeSchafer <77408854+GeorgeSchafer@users.noreply.github.com>
2024-02-29 09:12:23 -0700

    added counter to correspondsto description

--- SchemaJS.chai.mjs
+++ SchemaJS.chai.mjs
@@ -64,7 +64,7 @@ function compileKeywords(ajv, schema){
 }
 
 function schemaCorresponds(subject, alias, target, bool=true){
-    const description = alias + does(bool) + `correspond to response`
+    const description = getCounter() + alias + does(bool) + `correspond to response`
 
     const correspondsTo = () => {
         it(description, () => {
