a5696bab00f9771bdf9424d87a562fe1b338bc26
GeorgeSchafer <77408854+GeorgeSchafer@users.noreply.github.com>
2024-02-29 09:27:14 -0700

    started Schema extension tests

---
+++ test/SchemaJS.ext.spec.mjs
@@ -0,0 +1,21 @@
+import {
+    threwError,
+    did,
+    does,
+    have,
+    is,
+    matches,
+    getCounter,
+    count,
+    valueMatch,
+    objectsMatch,
+    throwsError,
+    nullCheck
+} from '../ChaiFunctions.mjs'
+import { Schema } from '../extensions/SchemaJS/Schema.mjs'
+
+describe('', () => {
+    describe('', () => {
+        
+    })
+})
