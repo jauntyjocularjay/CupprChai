744967923224aacf2340a66061dc34998265db7e
GeorgeSchafer <77408854+GeorgeSchafer@users.noreply.github.com>
2024-02-29 09:46:04 -0700

    Releasing changes

--- test/SchemaJS.ext.spec.mjs
+++ test/SchemaJS.ext.spec.mjs
@@ -10,12 +10,26 @@ import {
     valueMatch,
     objectsMatch,
     throwsError,
-    nullCheck
+    nullCheck,
+    SchemaTypeValue,
+    SchemaTypeProperty
 } from '../ChaiFunctions.mjs'
 import { Schema } from '../extensions/SchemaJS/Schema.mjs'
 
-describe('', () => {
-    describe('', () => {
-        
+describe('Schema Extension', () => {
+    describe('SchemaTypeValue()', () => {
+        const boolean = Schema.boolean
+        let target = {type:'boolean'}
+        SchemaTypeValue(boolean, target)
+        target = {type:'object'}
+        SchemaTypeValue(boolean, target, false)
+    })
+
+    describe('SchemaTypeProperty()', () => {
+        let target = 'object'
+        const schema = new Schema()
+        SchemaTypeProperty(schema, target)
+        schema.type = 'array'
+        SchemaTypeProperty(schema, target, false)
     })
 })
