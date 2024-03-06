6156ff6df4ddcc215ffc8335c80e3db31b4f4ec8
GeorgeSchafer <77408854+GeorgeSchafer@users.noreply.github.com>
2024-03-06 10:45:23 -0700

    adjusted Schematype functions

--- SchemaJS.ext.mjs
+++ SchemaJS.ext.mjs
@@ -30,7 +30,7 @@ function SchemaTypeValue(SchemaType, obj, bool=true){
     }
     const description = getCounter() + `SchemaType ${is(bool)} {${result.key}: '${result.value}'}`
 
-    valueMatch(SchemaType, obj, bool, description)
+    valueMatch(SchemaType.v(), obj, bool, description)
 }
 
 function SchemaTypeProperty(schema, alias, target, bool=true){
