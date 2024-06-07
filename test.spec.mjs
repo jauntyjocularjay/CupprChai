c4904e7aeed31bc472cf7f1627509694e27b3df5
jauntyjocularjay <77408854+jauntyjocularjay@users.noreply.github.com>
2024-06-07 03:45:22 -0700

    added mjs boilerplate

---
+++ test.spec.mjs
@@ -0,0 +1,25 @@
+import {
+    throwsAnError,
+    did,
+    does,
+    have,
+    has,
+    is,
+    matches,
+    getCounter,
+    expectToBeTrue,
+    expectValuesToMatch,
+    objectsAreEquivalent,
+    expectObjectsAreEqual,
+    expectToBeNull,
+    throwsError,
+    expectConstructorToThrowError,
+    nullCheck,
+} from 'Chai.mjs'
+import { expect } from 'chai'
+
+
+
+
+
+
